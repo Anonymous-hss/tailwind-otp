528ec553c99d66b0cfcc63d713cfa88acea3bd23
Anonymous-hss <harshalsawatkar24@gmail.com>
2025-01-03 09:45:32 +0530

    unseccesful attempt

--- src/components/otp.jsx
+++ src/components/otp.jsx
@@ -1,31 +1,57 @@
 import { useRef } from "react";
 import { useState } from "react";
+import { Button } from "./Buttons";
 
-export function Otp() {
-  const ref1 = useRef();
-  const ref2 = useRef();
-  const ref3 = useRef();
-  const ref4 = useRef();
-  const ref5 = useRef();
-  const ref6 = useRef();
-
+export function Otp({ number }) {
   const [disabled, setDisabled] = useState(true);
   return (
     <div className="flex justify-center">
-      <SubotpBox reference={ref1}></SubotpBox>
-      <SubotpBox></SubotpBox>
-      <SubotpBox></SubotpBox>
-      <SubotpBox></SubotpBox>
-      <SubotpBox></SubotpBox>
-      <SubotpBox></SubotpBox>
+      {Array(number).map((x) => (
+        <SubotpBox
+          reference={ref1}
+          onDone={() => {
+            ref2.current.focus();
+          }}
+        />
+      ))}
+      <br />
+      <Button disabled={disabled}> Sign up </Button>
     </div>
   );
 }
 
-function SubotpBox() {
+function SubotpBox({ reference, onDone, onBack }) {
+  const [inputvalue, setinputValue] = "";
   return (
     <div>
       <input
+        onKeyUp={(e) => {
+          if (e.key == "Backspace") {
+            onBack();
+          }
+        }}
+        value={inputvalue}
+        ref={reference}
+        onChange={(e) => {
+          const val = e.target.value;
+          if (
+            val == "1" ||
+            val == "2" ||
+            val == "3" ||
+            val == "4" ||
+            val == "5" ||
+            val == "6" ||
+            val == "7" ||
+            val == "8" ||
+            val == "9" ||
+            val == "0"
+          ) {
+            setinputValue(val);
+            onDone();
+          } else {
+          }
+          onDone();
+        }}
         type="text"
         className=" m-2 w-[40px] h-[50px] rounded-2xl bg-blue-500 outline-none px-4 text-white"
       />
